abfcc482c8b3884def71a487e338f0a077d6629e
Behn <beh028@csiro.au>
2020-03-25 10:11:54 +1100

    working script for joining

---
+++ Scripts/script_final.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(dplyr)
+
+whip_sir_data <- read_csv("data/raw_report_of_voice_calls.csv")
+sap_data <- read_csv("data/SAP_CONTACTINFO.CSV")
+
+
+
+whipsir_cut2 <- whip_sir_data %>% 
+select("AdditionalTeamName", "Last Updated Time", "Created Time", "Message Subject",  "Message Sent Time",
+       "Response Channel", "Voice Sent Time", "Voice Received Time", "Voice Acknowledged Time", "Response")
+
+
+whisper <- mutate(whipsir_cut2, row_num = rownames(whipsir_cut2))
+  
+
+clean2 <- left_join(whisper, sap_data, by = c ("AdditionalTeamName" = "Ident"))
+
