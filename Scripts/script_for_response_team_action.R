61c0abf06b199497d6a9d5df608148c35900fff7
Behn <beh028@csiro.au>
2020-03-25 15:49:28 +1100

    start of script for responce team data set

---
+++ Scripts/script_for_response_team_action.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(dplyr)
+
+stage_one <- read_csv("data/stage_one_sms.csv")
+stage_two <- read_csv("data/stage_two_phone_calls.csv")
+sap_data <- read_csv("data/SAP_CONTACTINFO.CSV")
