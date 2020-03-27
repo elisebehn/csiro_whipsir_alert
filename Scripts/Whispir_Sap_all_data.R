7511a071b96fde0164cf241a60e2460ee7e2107f
Behn <beh028@csiro.au>
2020-03-27 12:09:36 +1100

    all actions for one script producing 4 outputs

--- Scripts/Whispir_Sap_all_data.R
+++ Scripts/Whispir_Sap_all_data.R
@@ -7,6 +7,7 @@ library(tidyverse)
 
 stage_one <- read_csv("data/stage_one_sms.csv")
 sap_data <- read_csv("data/SAP_CONTACTINFO.CSV")
+stage_two <- read_csv("data/stage_two_phone_calls.csv")
 
 
 #select only useful information from these data sources
@@ -28,7 +29,31 @@ missing_from_whisper <- filter(remove_duplicate, is.na(FirstName))
   
 missing_from_SAP <- filter(remove_duplicate, is.na(Surname))
 
+#********************************
 
+
+response <- stage_two %>% 
+  select("AdditionalTeamName", "Last Updated Time", "Created Time", "Message Subject",  "Message Sent Time",
+         "Response Channel", "Voice Sent Time", "Voice Received Time", "Voice Acknowledged Time", "Response" )
+
+no_response <- filter(response, is.na(Response))
+
+cleaned <- left_join(no_response, sap_data, by = c ("AdditionalTeamName" = "Ident"))
+
+remove_duplicates_no_response <- cleaned[!duplicated(cleaned$AdditionalTeamName),]
+
+data_for_HR <- remove_duplicates_no_response %>% 
+  rename(EmployeeIdent = AdditionalTeamName) %>% 
+  mutate(Row_ID = 1:n()) %>% 
+  select(Row_ID, everything()) %>% 
+  mutate(comments = " ")
+
+sort_hr_data <- data_for_HR [c(1,2,12:43,3:11)]
+
+
+
+
+write_csv(sort_hr_data, path = "processed_data/Final_Data_for_response_Team.csv")
 write_csv(remove_duplicate, path = "processed_data/Final_data_output_all_data.csv")
 write_csv(missing_from_whisper, path = "processed_data/Final_data_missing_from_whispir.csv")
 write_csv(missing_from_SAP, path = "processed_data/Final_data_missing_from_sap.csv")
