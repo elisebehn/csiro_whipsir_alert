2bcdec49603db336c515b77e15c53bc796549a71
Behn <beh028@csiro.au>
2020-03-30 15:47:57 +1100

    latest script to use for combining data from Whispir

---
+++ Scripts/Latest_script_Whispir_reporting.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+
+
+stage_one <- read_csv("data/stage_one_sms.csv")
+sap_data <- read_csv("data/SAP_CONTACTINFO.CSV")
+stage_two <- read_csv("data/stage_two_phone_calls.csv")
+
+#incorporate SAP data to joined data set with contact details
+combined_data <- full_join(sap_data,stage_one_cut, by = c ("Ident" = "AdditionalTeamName"))
+
+remove_duplicate <- combined_data[!duplicated(combined_data$Ident),]
+
+
+missing_from_whisper <- filter(remove_duplicate, is.na(FirstName))
+
+missing_from_SAP <- filter(remove_duplicate, is.na(Surname))
+
+
+
+
+filter_response_stage_1 <- filter(stage_one, !is.na(Response)) %>% 
+  mutate(responded = "stage 1") %>% 
+  mutate(Response = as.numeric(Response))
+
+
+filter_response_stage_2 <- filter(stage_two, !is.na(Response)) %>% 
+  mutate(responded = "stage 2")
+
+
+people_who_responded <- bind_rows(filter_response_stage_1,filter_response_stage_2) %>% 
+  mutate(Response_received = "yes") %>% 
+  select("AdditionalTeamName","responded","Response_received")
+
+
+remove_dup_test <- people_who_responded[!duplicated(people_who_responded$AdditionalTeamName),]
+
+stage_one_slice <- stage_one %>% 
+  select("AdditionalTeamName","FirstName",	"LastName",
+         "Last Updated Time",	"Created Time",	"Message Label","Message Subject"	,
+         "Message Sent Time","Voice Sent Time")
+
+
+
+results_from_whispir_both_stages <- left_join(stage_one_slice, remove_dup_test, by = c ("AdditionalTeamName"))
+
+
+
+Response_team_report <- left_join(sap_data,results_from_whispir_both_stages, by = c ("Ident" = "AdditionalTeamName")) %>% 
+  filter(is.na(Response_received)) %>% 
+  filter(!is.na(FirstName)) %>% 
+  filter(!is.na(Surname)) %>% 
+  select(-"responded", -"Response_received") %>% 
+  mutate(Row_ID = 1:n()) %>% 
+  select(Row_ID, everything()) %>% 
+  mutate(comments = " ")
+
+
+write_csv(Response_team_report, path = "processed_data/Final_Data_for_response_Team.csv")
+write_csv(remove_duplicate, path = "processed_data/Final_data_output_all_data.csv")
+write_csv(missing_from_whisper, path = "processed_data/Final_data_missing_from_whispir.csv")
+write_csv(missing_from_SAP, path = "processed_data/Final_data_missing_from_sap.csv")
+
+summary()
+
+
