ecfc92a0620ac3d142ac5e9bb5eaabd5cfe5ae86
Donskoi <don19q@csiro.au>
2020-04-01 14:37:16 +1000

    Luci edits- changed filters removed firstname surname filters added ATNF user filter added notes removed a duplicate step for Elise to check.

--- Scripts/Latest_script_raw_Whispir_data.R
+++ Scripts/Latest_script_raw_Whispir_data.R
@@ -11,10 +11,11 @@ stage_one <- read_csv("data/stage_one_sms.csv")
 sap_data <- read_csv("data/SAP_CONTACTINFO.CSV")
 stage_two <- read_csv("data/stage_two_phone_calls.csv")
 
-stage_one_cut <- stage_one %>% 
-  select("AdditionalTeamName","FirstName",	"LastName",
-         "Last Updated Time",	"Created Time",	"Message Label","Message Subject"	,
-         "Message Sent Time","Voice Sent Time")
+#the below is a duplicate to stage_one_slice and not used- would delete?
+#stage_one_cut <- stage_one %>% 
+ # select("AdditionalTeamName","FirstName",	"LastName",
+       #  "Last Updated Time",	"Created Time",	"Message Label","Message Subject"	,
+       #  "Message Sent Time","Voice Sent Time")
 
 
 
@@ -58,20 +59,31 @@ results_from_whispir_both_stages <- left_join(stage_one_slice, remove_dup_test, 
 
 
 
-Response_team_report <- left_join(sap_data,results_from_whispir_both_stages, by = c ("Ident" = "AdditionalTeamName")) %>% 
-  filter(is.na(Response_received)) %>% 
-  filter(!is.na(FirstName)) %>% 
-  filter(!is.na(Surname)) %>% 
+Response_team_report <- left_join(sap_data,results_from_whispir_both_stages, by = c ("Ident" = "AdditionalTeamName"))%>% 
+  filter(is.na(Response_received)) #%>% 
+  
+  #The step below takes anyone who didnt make it into the whispir system out- these people are still relevant for the response team,
+  #should still be contacted
+  #filter(!is.na(FirstName)) %>% 
+
+  #The step below does nothing so would remove
+#filter(!is.na(Surname))
+
+  Response_team_report2 <- Response_team_report %>%   
   select(-"responded", -"Response_received") %>% 
   mutate(Row_ID = 1:n()) %>% 
   select(Row_ID, everything()) %>% 
-  mutate(comments = " ")
+  mutate(comments = " ") %>% rename (Employee_Subgroup = 'Employee Subgroup') %>% filter(Employee_Subgroup != 'ATNF User')
 
-
-write_csv(Response_team_report, path = "processed_data/Final_Data_for_response_Team.csv")
+  #Last 2 steps above I added a filter to remove ATNF Users#
+  
+write_csv(Response_team_report2, path = "processed_data/Final_Data_for_response_Team.csv")
 write_csv(remove_duplicate, path = "processed_data/Final_data_output_all_data.csv")
 write_csv(missing_from_whisper, path = "processed_data/Final_data_missing_from_whispir.csv")
 write_csv(missing_from_SAP, path = "processed_data/Final_data_missing_from_sap.csv")
 
-summarise(Response_team_report, num_rows = n())
+summarise(Response_team_report2, num_rows = n())
+
+#Actions from here- format the Final_Data_for_response_Team in excel- where phone numbers exit change column format to numeric, zero decimal
+#so you dont see exponential numbers. Bold the header. Send to Agnese. Check the missing from sap extract- these should all be ceased.
 
